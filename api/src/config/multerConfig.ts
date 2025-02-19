2a8438c1a38021b17250647e19c00c92acd36942
adharainfinita <adharanosalevich@gmail.com>
2025-02-19 12:23:30 -0300

    multerConfig chanche

--- api/src/config/multerConfig.ts
+++ api/src/config/multerConfig.ts
@@ -1,14 +1,7 @@
-// src/multerConfig.js
-import multer from 'multer';
+import multer from "multer";
 
-export const upload = multer({
-  dest: 'uploads/',
-  fileFilter: (req, file, cb) => {
-    const allowedMimes = ['text/plain', 'application/pdf'];
-    if (!allowedMimes.includes(file.mimetype)) {
-      return cb(new Error('Invalid file type') as unknown as null, false);
-    }
-    cb(null, true);
-  },
-});
+const storage = multer.memoryStorage(); // Guarda archivos en la RAM temporalmente
 
+const upload = multer({ storage });
+
+export default upload;
