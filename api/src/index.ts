952e0b79c6b3d0f456bb39ddd838f4444260a18e
adharainfinita <adharanosalevich@gmail.com>
2025-02-19 08:26:17 -0300

    cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import morgan from "morgan";
 import mailRoutes from "./routes/mailRoutes.js";
+import cors from 'cors';
 
 process.loadEnvFile();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 // Ruta principal
