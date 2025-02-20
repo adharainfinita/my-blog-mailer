41e83168498b678771fce831a6842fffa77c6c50
adharainfinita <adharanosalevich@gmail.com>
2025-02-19 23:17:27 -0300

    asdad

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   process.loadEnvFile();
-  const MONGO_URI = process.env.MONGO_URI || "";
+
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "");
     console.log("🔥 MongoDB connected");
   } catch (error) {
     console.error("❌ Error connecting to MongoDB:", error);
